98a60f7d8d8d8f5685abe6280b79fe23793e5dca
HuseinliAli <ali.guseynoov@gmail.com>
2022-12-12 01:22:43 +0400

    v.0.5.0

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Navbar extends Component {
   render() {
-    return <div>Navbar</div>;
+    return (
+      <div>
+        <h3>Navbar</h3>
+      </div>
+    );
   }
 }
